6206d264d2dca207287515856cb67c8cf44001c9
Anil Kumar Musinada <121026687+Musinada@users.noreply.github.com>
2023-06-29 00:37:54 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,24 +1,24 @@
 variable "region" {
-  default     = "us-east-1"
+  default     = "ap-south1"
   description = "AWS region"
 }
 
 variable "ssh_key" {
   description = "ssh-key to login to the instances"
-  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDZFuuu7wQCfVl3kHpnLPFfYTaSQ74IhEYV+Q2LXee6s0jtJsAo6BlZ7GUkPm6qa7Z+O2jXphe8VQsB5btMxv1Us5xS+AyIlPWFC1pRAsoJULmg8Tc7s97+l6BPzVwfDhFqIZkZpRnY7KYb438+/wA9ahdfeYb1BiptN/VUfGz12gmJhppDh5/MyyXUhwUrmLDSnsxr+VyLTfeffMhz2KXGF3UR3mjgPklIMpwMQJPoM6rEUJ4zpz+cVJLBRAQT0QXZMvjd+eNxg6bgWlnHYvK/1KdQDY6q/KaKMKJqHVNYRMpRgcBnfyHhiPP5ePzcB1aQgHJl9uwyTkwjcukvJZw7 cicd"
+  default = ""
 }
 
 variable "subnets" {
   description = "subnets for workers"
-  default = ["subnet-0c1622b82b3c369e3", "subnet-0a76e2183953fdd87"]
+  default = ["subnet-0843de099cd275280", "subnet-006aa011a526f0c48"]
 }
 
 variable "vpc" {
   description = "VPC ID"
-  default = "vpc-06d9c1d3ecf203e11"
+  default = "vpc-0712f5b29ecc1e69f"
 }
 
 variable "instance_type" {
   description = "Instance Type"
-  default = "r5.2xlarge"
+  default = "t2.micro"
 }
